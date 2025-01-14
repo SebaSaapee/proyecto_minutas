5b4e8303fd4e5f136d137938b2faca457c8e7037
Sebastian <sebastian.molina01@alumnos.ucn.cl>
2025-01-14 13:29:30 -0300

    PRUEBA2

--- src/menudiario/menudiario.service.ts
+++ src/menudiario/menudiario.service.ts
@@ -5,79 +5,76 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IMenudiario } from 'src/common/interfaces/menudiario.interface';
 import { IIngredientexplato } from 'src/common/interfaces/ingredientexplato.interface';
-import { IPlato } from 'src/common/interfaces/plato.interface';  // Importa la interfaz del plato
+import { IPlato } from 'src/common/interfaces/plato.interface';
 
 @Injectable()
 export class MenuDiarioService {
   constructor(
     @InjectModel(MENUDIARIO.name)
     private readonly model: Model<IMenudiario>,
-    @InjectModel(INGREDIENTEXPLATO.name)
-    private readonly ingredientexplatoModel: Model<IIngredientexplato>,
-    @InjectModel(PLATO.name)  // Asegúrate de que 'Plato' esté correctamente configurado en tu base de datos
-    private readonly platoModel: Model<IPlato>,
+     @InjectModel( INGREDIENTEXPLATO.name) private readonly ingredientexplatoModel: Model<IIngredientexplato>,
+     @InjectModel(PLATO.name) // Inyectamos el modelo de Plato para hacer consultas sobre ellos
+     private readonly platoModel: Model<IPlato>,  
   ) {}
 
   // Crear menú asegurándose de que no se repita en 12 semanas
   async create(menuDTO: MenuDTO): Promise<IMenudiario> {
     const last12Weeks = new Date();
     last12Weeks.setDate(last12Weeks.getDate() - 84); // 12 semanas atrás
-
+  
     const last4Weeks = new Date();
     last4Weeks.setDate(last4Weeks.getDate() - 28); // 4 semanas atrás
-
-    // Obtener detalles de los platos desde la base de datos
-    const platos = await this.platoModel.find({ '_id': { $in: menuDTO.listaplatos } }).exec();
-
-    // Filtrar los platos por categorías
-    const platosFondo = platos.filter(plato => plato.categoria === 'PLATO DE FONDO');
-    const platosRestrictivos = platos.filter(plato => 
+  
+    // Obtener todos los platos por su ID
+    const platos = await this.platoModel.find({ 
+      '_id': { $in: menuDTO.listaplatos } // Filtramos los platos por los IDs de la lista
+    });
+  
+    // Filtramos los platos según su categoría
+    const platosFondoIds = platos.filter(plato => plato.categoria === 'PLATO DE FONDO').map(plato => plato._id);
+    const platosRestrictivosIds = platos.filter(plato =>
       ['VEGETARIANO', 'VEGANA', 'GUARNICIÓN'].includes(plato.categoria)
-    );
-    const otrosPlatos = platos.filter(plato => 
+    ).map(plato => plato._id);
+  
+    const otrosPlatosIds = platos.filter(plato =>
       !['PLATO DE FONDO', 'VEGETARIANO', 'VEGANA', 'GUARNICIÓN'].includes(plato.categoria)
-    );
-
-    // Verificar si los platos de fondo ya existen en los últimos 12 semanas
-    const existingPlatosFondo = await this.model.findOne({
+    ).map(plato => plato._id);
+  
+    // Verificar que no se repitan platos de fondo en las últimas 12 semanas
+    const existingPlatoFondo = await this.model.findOne({
       createdAt: { $gte: last12Weeks },
-      'listaplatos._id': { $in: platosFondo.map(plato => plato._id) },
+      listaplatos: { $in: platosFondoIds }, // Comprobamos si hay algún plato de fondo repetido
     });
-
-    if (existingPlatosFondo) {
+  
+    if (existingPlatoFondo) {
       throw new BadRequestException(
-        'Un menú con estos platos de fondo ya existe en las últimas 12 semanas.',
+        'Un menú con los mismos platos de fondo ya existe en las últimas 12 semanas.',
       );
     }
-
-    // Verificar si los platos de las categorías restrictivas (VEGETARIANO, VEGANA, GUARNICIÓN) ya existen en los últimos 4 semanas
-    const existingPlatosRestrictivos = await this.model.findOne({
+  
+    // Verificar que no se repitan platos restrictivos (vegetariano, vegano, guarnición) en las últimas 4 semanas
+    const existingPlatoRestrictivo = await this.model.findOne({
       createdAt: { $gte: last4Weeks },
-      'listaplatos._id': { $in: platosRestrictivos.map(plato => plato._id) },
+      listaplatos: { $in: platosRestrictivosIds }, // Comprobamos si hay algún plato restrictivo repetido
     });
-
-    if (existingPlatosRestrictivos) {
+  
+    if (existingPlatoRestrictivo) {
       throw new BadRequestException(
-        'Un menú con uno o más platos de las categorías restrictivas (Vegetariano, Vegano, Guarnición) ya existe en las últimas 4 semanas.',
+        'Un menú con los mismos platos vegetarianos, veganos o de guarnición ya existe en las últimas 4 semanas.',
       );
     }
-
-    // Verificar si algún plato (de fondo, vegetariano, vegano, o guarnición) ya existe en los últimos 4 semanas
-    const allPlatos = [...platosFondo, ...platosRestrictivos];
-    const existingPlatos = await this.model.findOne({
-      createdAt: { $gte: last4Weeks },
-      'listaplatos._id': { $in: allPlatos.map(plato => plato._id) },
-    });
-
-    if (existingPlatos) {
-      throw new BadRequestException('Un menú con estos platos ya existe en las últimas 4 semanas.');
+  
+    // Verificar que no haya platos repetidos en todo el menú (todos los platos por su ID)
+    const allPlatosIds = [...platosFondoIds, ...platosRestrictivosIds, ...otrosPlatosIds];
+    const platosUnicos = new Set(allPlatosIds.map(platoId => platoId.toString())); // Usamos Set para eliminar duplicados
+    if (platosUnicos.size !== allPlatosIds.length) {
+      throw new BadRequestException('Hay platos repetidos en el menú.');
     }
-
-    // Si no existe ningún menú duplicado, crear el nuevo menú
+  
+    // Si no se encontraron duplicados, creamos el nuevo menú
     const newMenu = new this.model(menuDTO);
     return await newMenu.save();
   }
-
   async findAll(): Promise<IMenudiario[]> {
     return await this.model.find().populate('listaplatos').populate('id_sucursal');
   }
@@ -124,7 +121,9 @@ export class MenuDiarioService {
       .populate('listaplatos');
   }
 
-  // Función para calcular ingredientes por período de tiempo y sucursal
+
+
+  //Funcion para calcular ingredientes x periodo de tiempo y sucursal.
   async calcularIngredientesPorPeriodo(
     filtro: { fechaInicio: Date; fechaFin: Date; sucursalId: string; platosConCantidad: { platoId: string; cantidad: number }[] },
   ): Promise<{
@@ -133,7 +132,7 @@ export class MenuDiarioService {
   }> {
     const ingredientesTotales: { [ingredienteId: string]: { nombreIngrediente: string; cantidad: number; unidadmedida: string } } = {};
     const ingredientesPorPlato: { [platoId: string]: { [ingredienteId: string]: { nombreIngrediente: string; cantidad: number; unidadmedida: string } } } = {};
-
+  
     // Obtener menús del período filtrado por sucursal
     const menus = await this.model
       .find({
@@ -142,33 +141,33 @@ export class MenuDiarioService {
       })
       .populate('listaplatos')
       .exec();
-
+  
     // Procesar cada menú
     for (const menu of menus) {
       for (const plato of menu.listaplatos) {
         // Obtener la cantidad del plato en la lista de platos con cantidad
         const platoConCantidad = filtro.platosConCantidad.find(p => p.platoId === plato._id.toString());
         const cantidadPlato = platoConCantidad ? platoConCantidad.cantidad : 0;
-
+  
         // Inicializa la entrada del plato si no existe
         if (!ingredientesPorPlato[plato._id]) {
           ingredientesPorPlato[plato._id] = {};
         }
-
+  
         // Obtener ingredientes del plato
         const ingredientesDelPlato = await this.ingredientexplatoModel
           .find({ id_plato: plato._id })
           .populate('id_ingrediente')
           .exec();
-
+  
         // Calcular ingredientes necesarios por plato, multiplicados por la cantidad de platos
         for (const ingredienteDelPlato of ingredientesDelPlato) {
           const { id_ingrediente, porcion_neta } = ingredienteDelPlato;
           const ingrediente = id_ingrediente as any;
-
+  
           if (porcion_neta) {
             const cantidadTotalIngrediente = porcion_neta * cantidadPlato;
-
+  
             // Sumar al total general de ingredientes
             if (ingredientesTotales[ingrediente._id]) {
               ingredientesTotales[ingrediente._id].cantidad += cantidadTotalIngrediente;
@@ -179,7 +178,7 @@ export class MenuDiarioService {
                 unidadmedida: ingrediente.unidadmedida,
               };
             }
-
+  
             // Sumar al desglose por plato
             if (ingredientesPorPlato[plato._id][ingrediente._id]) {
               ingredientesPorPlato[plato._id][ingrediente._id].cantidad += cantidadTotalIngrediente;
@@ -194,7 +193,12 @@ export class MenuDiarioService {
         }
       }
     }
-
-    return { ingredientesTotales, ingredientesPorPlato };
+  
+    return {
+      ingredientesTotales,
+      ingredientesPorPlato,
+    };
   }
+  
+  
 }
