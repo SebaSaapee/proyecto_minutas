c1b866cf68176ef9c347614a7e8a2957e4d8f56a
Sebastian <sebastian.molina01@alumnos.ucn.cl>
2025-01-14 11:48:08 -0300

    fecha unica

--- src/menudiario/schema/menudiario.schema.ts
+++ src/menudiario/schema/menudiario.schema.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 export const MenuDiarioSchema = new mongoose.Schema(
   {
     nombre: { type: String, required: true },
-    fecha :{type: Date, required: true},
+    fecha :{type: Date, required: true,unique: true,},
     semana: { type: Number, required: true }, // Campo para número de semana
     id_sucursal: { type: mongoose.Schema.Types.ObjectId, ref: 'sucursales', required: true },
 
