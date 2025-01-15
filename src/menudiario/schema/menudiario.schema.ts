138e427100857a1e0e2b3af1f5d2c232584c1f98
Sebastian <sebastian.molina01@alumnos.ucn.cl>
2025-01-15 10:49:13 -0300

    fecha unica 2.0

--- src/menudiario/schema/menudiario.schema.ts
+++ src/menudiario/schema/menudiario.schema.ts
@@ -1,10 +1,9 @@
-
 import * as mongoose from 'mongoose';
 
-export const MenuDiarioSchema = new mongoose.Schema(
+const MenuDiarioSchema = new mongoose.Schema(
   {
     nombre: { type: String, required: true },
-    fecha: { type: Date, required: true, unique: false }, // La fecha ya no es única aquí
+    fecha: { type: Date, required: true }, // Fecha simple, sin restricción de unicidad directa aquí
     semana: { type: Number, required: true }, // Campo para número de semana
     id_sucursal: { type: mongoose.Schema.Types.ObjectId, ref: 'sucursales', required: true },
     estado: { type: String, required: true },
@@ -13,5 +12,10 @@ export const MenuDiarioSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-// Índice compuesto para garantizar que la combinación de fecha, semana e id_sucursal sea única
+// Índice para garantizar que la fecha sea única
+MenuDiarioSchema.index({ fecha: 1 }, { unique: true });
+
+// Índice compuesto para evitar duplicados en fecha, semana e id_sucursal
 MenuDiarioSchema.index({ fecha: 1, semana: 1, id_sucursal: 1 }, { unique: true });
+
+export { MenuDiarioSchema };
