657c578af83b00854bd1a6078a3aeaf76a9290e6
Cokke <jorgerandomgamer@gmail.com>
2025-02-27 10:37:34 -0300

    Update proyecciones

--- src/proyeccion/proyeccion.controller.ts
+++ src/proyeccion/proyeccion.controller.ts
@@ -6,6 +6,7 @@ import {
     Param,
     Post,
     Put,
+    Query,
     Res,
     UseGuards,
   } from '@nestjs/common';
@@ -47,68 +48,73 @@ import {
     delete(@Param('id') id: string) {
       return this.proyeccionService.delete(id);
     }
+
     @Get(':id/reporte-insumos')
-    async generateIngredientReport(@Param('id') proyeccionId: string, @Res() res: Response) {
-      try {
-        // Generar el reporte usando el servicio
-        const report = await this.proyeccionService.generateIngredientReportForProjection(proyeccionId);
-  
-        // Crear un nuevo libro de Excel
-        const workbook = new ExcelJS.Workbook();
-        const worksheet = workbook.addWorksheet('Reporte de Ingredientes');
-  
-        // Añadir encabezados
-        const headers = ['Fecha Inicio', 'Fecha Fin', 'Ingrediente', 'Cantidad Total', 'Unidad de Medida'];
-        const headerRow = worksheet.addRow(headers);
-        headerRow.eachCell((cell) => {
+  async generateIngredientReport(
+    @Param('id') proyeccionId: string,
+    @Res() res: Response,
+    @Query('fecha') fecha?: string,
+  ) {
+    try {
+      // Generar el reporte usando el servicio
+      const report = await this.proyeccionService.generateIngredientReportForProjection(proyeccionId, fecha? fecha : null);
+
+      // Crear un nuevo libro de Excel
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet('Reporte de Ingredientes');
+
+      // Añadir encabezados
+      const headers = ['Fecha Inicio', 'Fecha Fin', 'Ingrediente', 'Cantidad Total', 'Unidad de Medida'];
+      const headerRow = worksheet.addRow(headers);
+      headerRow.eachCell((cell) => {
+        cell.fill = {
+          type: 'pattern',
+          pattern: 'solid',
+          fgColor: { argb: 'FFD3D3D3' }, // Gris claro para el encabezado
+        };
+        cell.font = { bold: true };
+      });
+
+      // Añadir datos al Excel
+      let rowIndex = 2; // Empezamos desde la fila 2 (debajo del encabezado)
+      for (const ingrediente of report.ingredientes) {
+        const row = worksheet.addRow([
+          report.fechaInicio.toISOString().split('T')[0], // Fecha inicio (YYYY-MM-DD)
+          report.fechaFin.toISOString().split('T')[0],    // Fecha fin (YYYY-MM-DD)
+          ingrediente.nombreIngrediente,
+          ingrediente.cantidadTotal,
+          ingrediente.unidadMedida || 'N/A', // Si no hay unidad de medida, mostrar "N/A"
+        ]);
+
+        // Alternar colores de fondo
+        const fillColor = rowIndex % 2 === 0 ? 'FFC6EFCE' : 'FFFFFFFF'; // Verde claro o blanco
+        row.eachCell((cell) => {
           cell.fill = {
             type: 'pattern',
             pattern: 'solid',
-            fgColor: { argb: 'FFD3D3D3' }, // Gris claro para el encabezado
+            fgColor: { argb: fillColor },
           };
-          cell.font = { bold: true };
         });
-  
-        // Añadir datos al Excel
-        let rowIndex = 2; // Empezamos desde la fila 2 (debajo del encabezado)
-        for (const ingrediente of report.ingredientes) {
-          const row = worksheet.addRow([
-            report.fechaInicio.toISOString().split('T')[0], // Fecha inicio (YYYY-MM-DD)
-            report.fechaFin.toISOString().split('T')[0],    // Fecha fin (YYYY-MM-DD)
-            ingrediente.nombreIngrediente,
-            ingrediente.cantidadTotal,
-            ingrediente.unidadMedida || 'N/A', // Si no hay unidad de medida, mostrar "N/A"
-          ]);
-  
-          // Alternar colores de fondo
-          const fillColor = rowIndex % 2 === 0 ? 'FFC6EFCE' : 'FFFFFFFF'; // Verde claro o blanco
-          row.eachCell((cell) => {
-            cell.fill = {
-              type: 'pattern',
-              pattern: 'solid',
-              fgColor: { argb: fillColor },
-            };
-          });
-  
-          rowIndex++;
-        }
-  
-        // Configurar ajustes de columna
-        worksheet.columns.forEach((column) => {
-          column.width = 25; // Ancho de columna
-        });
-  
-        // Enviar el archivo Excel como respuesta
-        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.setHeader('Content-Disposition', 'attachment; filename=reporte_ingredientes.xlsx');
-  
-        // Escribir el archivo Excel en la respuesta
-        await workbook.xlsx.write(res);
-        res.end();
-      } catch (error) {
-        console.error('Error al generar el reporte:', error);
-        res.status(500).send('Error al generar el reporte');
+
+        rowIndex++;
       }
+
+      // Configurar ajustes de columna
+      worksheet.columns.forEach((column) => {
+        column.width = 25; // Ancho de columna
+      });
+
+      // Enviar el archivo Excel como respuesta
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      res.setHeader('Content-Disposition', 'attachment; filename=reporte_ingredientes.xlsx');
+
+      // Escribir el archivo Excel en la respuesta
+      await workbook.xlsx.write(res);
+      res.end();
+    } catch (error) {
+      console.error('Error al generar el reporte:', error);
+      res.status(500).send('Error al generar el reporte');
     }
   }
+}
   
--- src/proyeccion/proyeccion.service.ts
+++ src/proyeccion/proyeccion.service.ts
@@ -7,6 +7,7 @@ import { ProyeccionDTO } from './dto/proyeccion.dto';
 import { IProyeccion } from 'src/common/interfaces/proyeccion.interface';
 import { IIngredientexplato } from 'src/common/interfaces/ingredientexplato.interface';
 import { IIngrediente } from 'src/common/interfaces/ingrediente.interface';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class ProyeccionService {
@@ -39,17 +40,29 @@ export class ProyeccionService {
       msg: 'Deleted',
     };
   }
-  async generateIngredientReportForProjection(proyeccionId: string): Promise<any> {
+  
+  async generateIngredientReportForProjection(proyeccionId: string, fecha?: string): Promise<any> {
     // Fetch the specific projection
     const projection = await this.model.findById(proyeccionId).populate('lista.platoid');
     if (!projection) {
         throw new Error('Proyección no encontrada');
     }
 
+    let filteredList = projection.lista;
+
+    // Si se proporciona una fecha, filtrar la lista de platos
+    if (fecha) {
+        const dateToCompare = new Date(fecha.split('-').reverse().join('-'));
+        filteredList = projection.lista.filter(item => {
+            const itemDate = new Date(item.fecha.split('-').reverse().join('-'));
+            return itemDate.toDateString() === dateToCompare.toDateString();
+        });
+    }
+
     const ingredientMap = new Map<string, { nombre: string; cantidad: number; unidadmedida: string }>();
 
     // Ordenar la lista por fecha (de menor a mayor)
-    const sortedList = projection.lista.sort((a, b) => {
+    const sortedList = filteredList.sort((a, b) => {
         const dateA = new Date(a.fecha.split('-').reverse().join('-')); // Convertir "DD-MM-YYYY" a "YYYY-MM-DD"
         const dateB = new Date(b.fecha.split('-').reverse().join('-'));
         return dateA.getTime() - dateB.getTime();
@@ -61,7 +74,6 @@ export class ProyeccionService {
 
     for (const item of sortedList) {
         const { platoid, cantidad } = item;
-
         // Fetch ingredients for the current dish
         const ingredients = await this.ingredientexplatoModel
             .find({ id_plato: platoid })
@@ -71,7 +83,6 @@ export class ProyeccionService {
             const { id_ingrediente, peso_bruto } = ingredientEntry;
             const ingrediente = id_ingrediente as IIngrediente;
             if (!ingrediente || !peso_bruto) continue;
-
             const totalCantidad = parseInt(cantidad) * peso_bruto;
             if (ingredientMap.has(ingrediente.nombreIngrediente)) {
                 ingredientMap.get(ingrediente.nombreIngrediente).cantidad += totalCantidad;
@@ -93,12 +104,11 @@ export class ProyeccionService {
     }));
 
     console.log(startDate, endDate, report);
-
     return {
         fechaInicio: startDate,
         fechaFin: endDate,
         ingredientes: report,
     };
-}
+  }
 }
 
